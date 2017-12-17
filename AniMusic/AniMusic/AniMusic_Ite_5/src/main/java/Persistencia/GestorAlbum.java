2cc218b6a67fa40607f56ac934406544f37c6206
FenriRuiz <Fernando.Ruiz6@alu.uclm.es>
2017-12-17 16:44:06 +0100

    Arreglo de Warnings

--- AniMusic/AniMusic/AniMusic_Ite_5/src/main/java/Persistencia/GestorAlbum.java
+++ AniMusic/AniMusic/AniMusic_Ite_5/src/main/java/Persistencia/GestorAlbum.java
@@ -1,11 +1,6 @@
 package Persistencia;
 
 import Dominio.Album;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
 public class GestorAlbum {
 	
--- AniMusic/AniMusic/AniMusic_Ite_5/src/main/java/Presentacion/IU_Reproductor.java
+++ AniMusic/AniMusic/AniMusic_Ite_5/src/main/java/Presentacion/IU_Reproductor.java
@@ -1,16 +1,5 @@
 package Presentacion;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-
 public class IU_Reproductor {
 
 }
