07c022d3c1a917c4feea3fadd26e287883b374b7
alejandro <alejandro@172.19.145.206>
2017-12-19 11:25:31 +0100

    Agregadas interfaces de Administrador y cliente a todas las iteraciones

--- AniMusic/AniMusic/AniMusic_Ite_4/src/main/java/Persistencia/GestorUsuarios.java
+++ AniMusic/AniMusic/AniMusic_Ite_4/src/main/java/Persistencia/GestorUsuarios.java
@@ -1,10 +1,17 @@
 package Persistencia;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 import Dominio.Usuario;
 
 public class GestorUsuarios {
 
 	private Usuario[] lista_usuario;
+	
 	public Usuario[] getLista_usuario() {
 		return lista_usuario;
 	}
@@ -13,10 +20,27 @@ public class GestorUsuarios {
 		this.lista_usuario = lista_usuario;
 	}
 	public void verUsuario(Usuario user) {
-
-	}
-	public void anadirUsuario(Usuario user) {
 		
+	}
+	public boolean anadirUsuario(Usuario user) {
+		//No es funcional, ya que implementaremos más adelante una bbdd
+		
+		/*try {
+			
+			FileWriter fw=new FileWriter("BBDD.txt",true);
+			PrintWriter pw=new PrintWriter(fw);
+			pw.println(user.getEmail());
+			pw.println(user.getPassword());
+			pw.println(user.getNombre());
+			pw.println(user.getApellidos());
+			pw.close();
+		*/	return true;
+		/*	
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		return false;*/
 	}
 	public void modificarUsuario(Usuario user) {
 		
@@ -24,8 +48,26 @@ public class GestorUsuarios {
 	public void eliminarUsuario(Usuario user) {
 	
 	}
-	public void autenticarUsuario(Usuario user) {
-
+	public boolean autenticarUsuario(Usuario user)   {
+		/*String pass = null,email = null;
+		try {
+			File f=new File("BBDD.txt");
+			Scanner read = new Scanner(f);
+			while(read.hasNextLine()) {
+				email=read.next();
+				pass=read.next();
+			}
+			read.close();
+		}catch (Exception ex){
+			ex.printStackTrace();
+		}
+		
+		if((user.getEmail().equals(email)) && (user.getPassword().equals(pass))){
+			return true;
+		}else {
+			return false;
+		}*/
+		return true;
 	}
 	
 }
