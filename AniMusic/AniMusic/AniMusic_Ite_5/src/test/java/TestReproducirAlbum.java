ff98f77a868aad589c720a88bab610bad11a9e22
jorgemm7 <jorgemm777@gmail.com>
2017-12-23 12:11:16 +0100

    Gestión de pruebas de reproducir album y cancion

---
+++ AniMusic/AniMusic/AniMusic_Ite_5/src/test/java/TestReproducirAlbum.java
@@ -0,0 +1,45 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import Dominio.Album;
+import Persistencia.GestorAlbum;
+
+public class TestReproducirAlbum {
+
+	private GestorAlbum gesalb;
+	private Album alb;
+	
+	@Before
+	public void setUp() throws Exception {
+		gesalb=new GestorAlbum();
+		alb=new Album("nombre", "artista");
+	}
+
+	@Test
+	public void testReproAlbExisteBBDD() {
+		alb.setNombre("Nevermind");
+		/* ¿
+		assertEquals(alb.getNombre(),gesalb.reproducirAlbum(alb));
+		*/
+		assertTrue(gesalb.reproducirAlbum(alb));
+		assertNotNull(gesalb.reproducirAlbum(alb));
+	}
+	@Test
+	public void testReproCanNoExisteBBDD() {
+		alb.setNombre("album");
+		assertFalse(gesalb.reproducirAlbum(alb));
+		assertNotNull(gesalb.reproducirAlbum(alb));
+	}
+	@Test
+	public void testCampoVacio() {
+		alb.setNombre("null");
+		assertFalse(gesalb.reproducirAlbum(alb));
+	}	
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
---
+++ AniMusic/AniMusic/AniMusic_Ite_5/src/test/java/TestReproducirCancion.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import Dominio.Cancion;
+import Persistencia.GestorCancion;
+
+
+public class TestReproducirCancion {
+
+	private GestorCancion gescan;
+	private Cancion can;
+	
+	@Before
+	public void setUp() throws Exception {
+		gescan=new GestorCancion();
+		can=new Cancion("nombre","artista");
+	}
+	@Test
+	public void testReproCanExisteBBDD() {
+		can.setNombre("Smell like teen Spirit");
+		/* ¿
+		assertEquals(can.getNombre(),gescan.reproducirCancion(can));
+		*/
+		assertTrue(gescan.reproducirCancion(can));
+		assertNotNull(gescan.reproducirCancion(can));
+	}
+	@Test
+	public void testReproCanNoExisteBBDD() {
+		can.setNombre("Smell like teen Spirit");
+		assertFalse(gescan.reproducirCancion(can));
+		assertNotNull(gescan.reproducirCancion(can));
+	}
+	@Test
+	public void testCampoVacio() {
+		can.setNombre("null");
+		assertFalse(gescan.reproducirCancion(can));
+	}	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+}
