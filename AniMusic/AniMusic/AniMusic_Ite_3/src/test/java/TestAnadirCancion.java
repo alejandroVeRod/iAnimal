4487441debe0c0981ca6272247dfc59aa9ceb9c3
FenriRuiz <Fernando.Ruiz6@alu.uclm.es>
2017-12-24 12:09:29 +0100

    Arreglado Test Añadir Album y Añadir Canción

--- AniMusic/AniMusic/AniMusic_Ite_3/src/test/java/TestAnadirCancion.java
+++ AniMusic/AniMusic/AniMusic_Ite_3/src/test/java/TestAnadirCancion.java
@@ -4,22 +4,16 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import Dominio.Album;
 import Dominio.Cancion;
-import Persistencia.GestorAlbum;
 import Persistencia.GestorCancion;
 
 public class TestAnadirCancion {
 	
 	private GestorCancion gSong;
-	private GestorAlbum gAlb;
 	private Cancion song;
-	private Album alb;
 	
 	@Before
 	public void setUp() throws Exception {
-		gAlb = new GestorAlbum();
-		alb=new Album("nombre", "artista", "fecha");
 		gSong = new GestorCancion();
 		song = new Cancion("nombre", "artista", "album", "fecha", "duracion");
 	}
@@ -30,26 +24,13 @@ public class TestAnadirCancion {
 		song.setFecha("10/04/2010");
 		assertFalse(gSong.anadirCancion(song));
 	}
-	@Test
-	public void testExisteAlbBBDD() {
-		alb.setNombre("Appetite for Destruction");
-		alb.setArtista("Guns N' Roses");
-		alb.setFecha("21/07/1987");
-		assertFalse(gAlb.anadirAlbum(alb));
-	}
+
 	@Test
 	public void testNoExisteCancionBBDD() {
 		song.setNombre("Another Day");
 		song.setArtista("Paul McCartney");
 		song.setFecha("01/02/1971");
 		assertTrue(gSong.anadirCancion(song));
-	}
-	@Test
-	public void testNoExisteAlbBBDD() {
-		alb.setNombre("Alguna basura mas");
-		alb.setArtista("Kiko Rivera");
-		alb.setFecha("10/09/2016");
-		assertTrue(gAlb.anadirAlbum(alb));
 	}
 	
 }
